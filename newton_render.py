b0fbaf8274470cf23f05d1e12e075d99b0fbae86
WilliamAllen <wallen2268@gmail.com>
2022-06-23 05:30:20 -0500

    First working attempt

--- newton_render.py
+++ newton_render.py
@@ -1,42 +1,87 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 680, 680
+size = width, height = 1920, 1080
 speed = [2, 2]
-black = 0, 0, 0
-red = 255, 0, 0
-green = 0, 225, 0
-blue = 0, 0, 255
+# black = 0, 0, 0
+# red = 255, 0, 0
+# green = 0, 225, 0
+# blue = 0, 0, 255
 
-screen = pygame.display.set_mode(size)
+max_itr = 20
+colors = [(x*255)/20 for x in range(0, 20)]
+
 
 # ball = pygame.image.load("intro_ball.gif")
 # ballrect = ball.get_rect()
 
+
+# Each vector is a column vector
+
+def reader(filename):
+    with open(filename) as f:
+        while True:
+            # read next character
+            char = f.read(1)
+            # if not EOF, then at least 1 character was read, and 
+            # this is not empty
+            if char:
+                yield char
+            else:
+                return
+                
+
+r = reader('out.txt')
+
+
 arr = []
+temp_arr = []
 
-import csv
-with open("out.txt", 'r') as file:
-    r = csv.reader(file, delimiter=' ')
-    for line in r:
-        arr = line
+for c in r:
+    # c = next(r)    
+    # print(c)
+    if(c == ' '):
+        continue
 
-print(arr)
+    if(c == '('):
+        continue
 
-while 1:
+    if(c == ')'):
+        arr.append(temp_arr.copy())
+        temp_arr.clear()
+        continue
+    
+    if(c == '#'):
+        continue
+    
+    if(c == 'f'):
+        temp_arr.append(-1)
+        continue
+
+    temp_arr.append(int(c))
+
+    
+# print(arr)
+
+
+screen = pygame.display.set_mode(size)
+
+
+painted = False
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    for i in range(0, height):
-        for j in range(0, width):
-            if(arr[i + j] == "BLACK"):
-                pygame.draw.rect(screen, black, pygame.Rect(i,j,1,1))
-            if(arr[i + j] == "RED"):
-                pygame.draw.rect(screen, red, pygame.Rect(i,j,1,1))
-            if(arr[i + j] == "GREEN"):
-                pygame.draw.rect(screen, green, pygame.Rect(i,j,1,1))
-            if(arr[i + j] == "BLUE"):
-                pygame.draw.rect(screen, blue, pygame.Rect(i,j,1,1))
+    if not painted:
+        for i in range(0, width):
+            for j in range(0, height):
+                itr = arr[i][j]
+                color = (colors[itr], colors[itr], colors[itr])
+                pygame.draw.rect(screen, color, pygame.Rect(i,j,1,1))
+    painted = True
+    
     # screen.fill(black)
     # screen.blit(ball, ballrect)
     pygame.display.flip()
+    pygame.time.wait(100)
--- newtons.rkt
+++ newtons.rkt
@@ -1,327 +1,130 @@
-; #!usr/bin/env racket
 #lang racket
-(require trace)
-; Newtons method:
-; x_n+1 = x_n - (f(x_n) / f'(x_n))
 
-; lim h-> 0 f'(x) = (f(x + h) - f(x))/h
-; set h = 0.00001 for now
+(define TOLERANCE 0.000001)
+
+(define RESOLUTION-X 1920)
+(define RESOLUTION-Y 1080)
+
+(define XMIN -2)
+(define XMAX 2)
+(define YMIN -2)
+(define YMAX 2)
+
+(define colors '((255 0 0) (0 255 0) (0 0 255)))
+
+(define FUNCTION (lambda (x) (- (expt x 3) 1)))
+(define ROOTS '(1+0i -0.5+0.866025403i -0.5-0.866025403i))
+(define MAX-ITR 20)
+
+; Generate an nxm vector of 0s
+; This will make a bunch of aliases, (i.e) each subvector will be a clone of each other, and what affects one will affect all
+; (define (gen-2d-vector n m)
+;     (make-vector n (make-vector m 0))
+; )
+
+(define (gen-matrix m n)
+    (build-vector m
+        (lambda (row)
+            (make-vector n 0)
+        )
+    )
+)
+
+
 (define (deriv f x) 
     (/ (- (f (+ x 0.000001)) (f x)) 0.000001)
 )
-; f(x) = sin x
-; (display (deriv sin 2.0))
-; (newline)
-; f(x) = sin x
-; (display (deriv sin 2.0))
-; (newline)
 
-; f(x) = cos x - x
-; (display (deriv (lambda (x) (- (cos x) x)) 2.0))
-; (newline)
-
-
-; Newtons method:
-; x_n+1 = x_n - (f(x_n) / f'(x_n))
-(define (newtons f n x0)
-    (cond
-        ((zero? n) x0)
-        (else
-            (let ((res (newtons f (- n 1) x0))) 
-                (- 
-                    res
-                    (/ 
-                        (f res)
-                        (deriv f res)
-                    )
-                )
-            )
-        )
-    )
-) 
-
-; (trace newtons)
-; (display (newtons (lambda (x) (- (cos x) x)) 30 1))
-; (newline)
-
-; (display (newtons (lambda (x) (+ (expt x 5) (expt x 2) x 2)) 30 100))
-; (newline)
-
-; (display (newtons (lambda (x) (sin x)) 30 2))
-; (newline)
-
-
-; Newtons method on complex numbers
-; 
-
-(define TOLERANCE 0.000001)
-(define NUM_ITERATIONS 20)
-(define GRID_SIZE 100)
-(define RESOLUTION 680)
-
-
-(define BLACK 0)
-(define RED 1)
-(define GREEN 2)
-(define BLUE 3)
-
-(define COLORS '(BLACK RED GREEN BLUE))
-
-; For every (x,y) in R^2, [bounded by 2.7 or so, in incremenets of 0.1]
-; Conduct newtons method with (x,y) as the initial condition
-; If the solution does not converge [within TOLERANCE] to an established root in n steps [n = 20]
-; Paint the pixel black
-; Else:
-; Paint the pxiel the color of the root it converged to
-
-
-; (define (itr-over-list min max fn)
-;   (if (>= max min)
-;     (begin (fn min) (loop (+ min 1) max (apply fn min)))
-;     (void))
-; )
-
-
-; (define (get-element-recurse lst i j)
-;     (cond 
-;         ((null? lst) '())
-;         ((= i j) (car lst))
-;         (else (set! j (+ 1 j)) (get-element-recurse (cdr lst) i j))
-;     )
-
-; )
-
-
-; (define (get-element lst i)
-;     (get-element-recurse lst i 0)
-; )
-
-
-
-; (display (get-element '(a b c) 0))
-; (loop 1 3 (display(get-element '(abc) 0)))
-
-; (display (loop 0 10 (lambda (x) (+ 2 x))))
+; Find the incremenet step for a line of length max - min with d subdivisions
+(define (find-step min-val max-val d)
+    ( / (- max-val min-val) (- d 1))
+)
 
 (define (within-threshold-to-roots z roots) 
-    (begin
-        (map 
-            (lambda (x)
-                (cond
-                    (   (and (< (abs(real-part(- z x))) TOLERANCE) (< (abs(imag-part(- z x)))  TOLERANCE))
-                        #t
-                    )
-                    (else #f) 
+    (map 
+        (lambda (x)
+            (cond
+                (   
+                    (and (< (abs(real-part(- z x))) TOLERANCE) (< (abs(imag-part(- z x)))  TOLERANCE))
+                    x
                 )
+                (else #f) 
             )
-        roots)
-    )
-)
-
-; (display (within-threshold-to-roots 3+2i '(3+2.00000000001i 4+1i 1+1i)))
-
-
-
-
-(define (get-element-recurse lst i j)
-    (cond 
-        ((null? lst) '())
-        ((= i j) (car lst))
-        (else (set! j (+ 1 j)) (get-element-recurse (cdr lst) i j))
-    )
-
-)
-
-
-(define (get-element lst i)
-    (get-element-recurse lst i 0)
-)
-
-
-; return #f if there are no roots that meet threshold
-; otherwise return the corresponding color
-(define (get-color thresholds)
-    (get-color-recurse thresholds 0)
-)
-
-
-
-(define (get-color-recurse thresholds j)
-    (cond 
-        ((null? thresholds) BLACK)
-        ((eq? #t (car thresholds)) (get-element COLORS j))
-        (else (set! j (+ 1 j)) (get-color-recurse (cdr thresholds) j))
-    )
-
-)
-
-; For each (x,y) in grid
-; paint the (x,y) coordinate
-; (define (newtons-complex-grid f roots) 
-;     ()
-; )
-
-; grid should be layed out like
-; ((complex_num complex_num complex_num complex_num) (complex_num complex_num complex_num complex_num) (complex_num complex_num complex_num complex_num) )
-
-; (define (paint-grid grid f roots)
-;     ()
-; )
-
-; This function will tell you what color this complex number should be
-(define (paint-newtons-complex f z roots) 
-    (let ((res (paint-newtons-complex-recurse f NUM_ITERATIONS z roots)))
-        (get-color (within-threshold-to-roots res roots))
-        ; ((not (boolean? (get-color (within-threshold-to-roots z roots)))) (get-color (within-threshold-to-roots z roots)))
-    )
-)
-
-
-(define (paint-newtons-complex-recurse f n z roots)
-    (cond
-        ((= 0 n) z)
-        (else
-            (set! z (paint-newtons-complex-recurse f (- n 1) z roots)) 
-            (- z (/ (f z) (deriv f z)))
         )
-    )
-)
-; (trace paint-newtons-complex-recurse)
-; (display (paint-newtons-complex (lambda (x) (- (expt x 3) 1)) 0.5+0.5i '(1+0i -0.5+0.866025403i -0.5-0.866025403i)))
-
-; (define newtons-complex)
-
-; (define (generate-newtons-fractal) ())
-
-(define (build-grid step xmin xmax ymin ymax) 
-    (build-grid-recurse step xmin xmax ymin ymax xmin ymin)
+    roots)
 )
 
 
+; Set an nxm vector to complex numbers w/ scaling factor
+; i.e -2 to 2 w/ 10 elements is a step of 4/10
+(define (make-complex-plane vec x y xstep ystep)
 
-(define (build-grid-recurse step xmin xmax ymin ymax x y)
-    (cond 
-        ((and (>= y ymax) (>= x xmax)) (list (make-rectangular x y))) ; End
-        ((>= x xmax)
-                ; (set! y (+ y step))
-                ; (set! x xmin)
-                (append (list (make-rectangular x y)) (build-grid-recurse step xmin xmax ymin ymax xmin (+ y step)) )
-                ; (build-grid-recurse step xmin xmax ymin ymax x (+ y step)) 
-                
-            
-        ) ; End line, increase y, reset x
+        (for ([i (in-range 0 (vector-length vec))])
+            (for ([j (in-range 0 (vector-length (vector-ref vec i)))])
+                    (vector-set! (vector-ref vec i) j (make-rectangular (+ x (* xstep i)) (+ y (* ystep j))))
+                    ; (display i) (display ",") (display j) (display "    ") (display (make-rectangular (+ x (* xstep i)) (+ y (* ystep j)))) (newline)
+                    ; (display (vector-ref vec 2)) (newline)
+            )
+        )
+)
+
+
+
+; (define (get-color-by-itr root itr max-itr)
+;     (map (list-ref COLORS (index-of ROOTS root))
+; )
+
+
+
+; Given a function, f a beginning point z, aribtrary roots roots, a maximum number of iteration max-itr and the current iteration itr
+; For 0 to itr
+; Run newtons method
+; Check for convergence
+; If convergence, return color modified by iteration count
+; If we are here, return black
+(define (newtons-method-paint-by-iteration f z roots max-itr itr)
+    (let([thresholds (within-threshold-to-roots z roots)])
+        (let ([filtered-thresholds (filter (lambda (x) (not (boolean? x))) thresholds)])
+            (cond 
+                ((> itr max-itr)
+                    #f
+                )
         
-        (else 
+                ((not (empty? filtered-thresholds))
+                    ; (get-color-by-itr (first filtered-thresholds) itr max-itr)
+                    itr
+                )
 
-            (append (list (make-rectangular x y)) (build-grid-recurse step xmin xmax ymin ymax (+ x step) y))
-            ; (display x)
-                
-
+                (else
+                    (newtons-method-paint-by-iteration f (- z (/ (f z) (deriv f z))) roots max-itr (+ itr 1))
+                )
+        
+            )
         )
-    ) ; Progress, add x+yi to list
+    )
 )
 
 
-; step = (xmax - xmin)/RES
 
+(define vector (gen-matrix RESOLUTION-X RESOLUTION-Y))
+; (display vector)
 (newline)
 
-(define grid (build-grid (/ 4 RESOLUTION) -2 2 -2 2 ))
-(define equation (lambda (x) (- (expt x 3) 1)))
-(define roots '(1+0i -0.5+0.866025403i -0.5-0.866025403i))
+; (vector-set! (vector-ref vector 0) 2 55)
+
+(make-complex-plane vector XMIN YMIN (find-step XMIN XMAX RESOLUTION-X) (find-step YMIN YMAX RESOLUTION-Y))
+; (display vector)
 (newline)
-
-; (display (list? grid))
 (newline)
-
-; (display grid)
-
 ; (display
-;     (map (lambda (x) (paint-newtons-complex equation x roots)) grid)
+;     (vector-map (lambda (col) (vector-map (lambda (x) (newtons-method-paint-by-iteration FUNCTION x ROOTS MAX-ITR 0)) col)) vector)
 ; )
-
-(set! grid (map (lambda (x) (paint-newtons-complex equation x roots)) grid))
+(newline)
+(newline)
 
 (with-output-to-file "out.txt"
-    (lambda () (print grid)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-; (build-grid 10)
-; (display (vector-map (make-vector 10) (lambda (x) (vector-set! x make-vector 10)) ))
-; 1-2-3-4-5-6-7-8-9-10||1-2-3-4-5-6-7-8-9-10
-; (define (build-grid-recurse step xmin xmax ymin ymax x y lst)
-;     (cond 
-;         ((>= y ymax) '()) ; End
-;         ((>= x xmax)
-;             (begin 
-;                 (+ y step)
-;                 (set! x xmin)
-;                 (append lst (list (make-rectangular x y)))
-;                 (build-grid-recurse step xmin xmax ymin ymax x (+ y step) lst) 
-                
-;             )
-;         ) ; End line, increase y, reset x
-        
-;         (else 
-;             (cons(build-grid-recurse step xmin xmax ymin ymax (+ x step) y lst))
-;         )
-;     ) ; Progress, add x+yi to list
-; )
-
-
-; (define (build-grid-recurse step res xmin xmax ymin ymax x y lst)
-;     (cond
-;         ((>= y ymax) lst)
-;         (#t (append (build-list res (lambda (x) (* x step))) (build-grid-recurse step res xmin) )
-;     )
-; )
-
-; (trace build-grid-recurse)
-(newline)
-; (define grid (build-grid 0.1 -1 1 -1 1))
-; (display (list? grid))
-; (display (car grid))
-; (map (lambda (x) (begin(display(x))(newline))) grid)
-
-; (display
-;     (build-list 5 
-;         (lambda (x) 
-
-;             ; (make-rectangular x x)
-;         )
-;     )
-; )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    (lambda () (print (vector-map (lambda (col) (vector-map (lambda (x) (newtons-method-paint-by-iteration FUNCTION x ROOTS MAX-ITR 0)) col)) vector))))
+    ; (vector-map (vector-map (lambda (x) (newtons-method-paint-by-iteration FUNCTION x ROOTS MAX-ITR 0)) x) vector)
+; 
+; (display (make-complex-plane vector XMIN YMIN (find-step XMIN XMAX RESOLUTION-X) (find-step YMIN YMAX RESOLUTION-YXMAX
+; Take a list and set every element to be
